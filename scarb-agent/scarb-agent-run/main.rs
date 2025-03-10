70d9648abbcdb90682575da6b38ce9ece022aa0c
0xTrinityy <trinity0x@protonmail.com>
2025-03-10 17:12:35 +0000

    feat: :bug: bump to cairo 2.11.1

--- scarb-agent/scarb-agent-run/main.rs
+++ scarb-agent/scarb-agent-run/main.rs
@@ -45,19 +45,13 @@ struct Args {
     #[clap(long)]
     append_return_values: Option<bool>,
 
-    #[clap(
-        long = "cairo-pie-output",
-        conflicts_with_all = ["proof-mode", "air-private-input", "air-public-input"]
-    )]
+    #[clap(long)]
     cairo_pie_output: Option<PathBuf>,
 
-    #[clap(long = "air-public-input", requires = "proof-mode")]
+    #[clap(long)]
     air_public_input: Option<PathBuf>,
 
-    #[clap(
-        long = "air_private_input",
-        requires_all = ["proof-mode", "trace-file", "memory-file"] 
-    )]
+    #[clap(long = "air_private_input")]
     air_private_input: Option<PathBuf>,
 
     #[clap(long)]
